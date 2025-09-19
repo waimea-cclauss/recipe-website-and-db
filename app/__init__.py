b40adb73e5e5d85afd88ffa1e1e039502a06c173
waimea-cclauss <cclauss@waimea.school.nz>
2025-09-19 12:54:15 +1200

    adding recipe+image fixed

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 #===========================================================
 
 from flask import Flask, render_template, request, flash, redirect
+from flask import send_from_directory
 import html
 
 from app.helpers.session import init_session
@@ -15,18 +16,13 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'app/static/uploads'
+UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
-
-
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-from flask import send_from_directory
 
 
 # Create the app
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, UPLOAD_FOLDER)
 
 # Configure app
 init_session(app)   # Setup a session for messages, etc.
@@ -121,19 +117,7 @@ def add_a_recipe():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('download_file', name=filename))
-    # https://flask.palletsprojects.com/en/stable/patterns/fileuploads/
-        
+
 @app.route('/uploads/<name>')
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
